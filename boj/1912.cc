ab0c02b6be9e8d7253b09b34e15b5c69850ad0dc
Huiseong Lim <105583102+limhuiseong@users.noreply.github.com>
2025-03-09 10:56:30 +0000

    https://www.acmicpc.net/problem/1912

---
+++ boj/1912.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+int a[100005];
+int d[100005];
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+        d[i] = a[i];
+    }
+    
+    for (int i = 2; i <= n; i++) {
+        d[i] = max(d[i], d[i - 1] + a[i]);
+    }
+
+    cout << *max_element(d + 1, d + n + 1) << '\n';
+}
